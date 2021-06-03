294eb201caebb9eaa1d77d4be35b1b916d2a7dd6
Arash Toyser <arash.toyser@vikinganalytics.se>
2021-06-03 13:04:36 +0200

    Fixed an issue with e2e testing.

--- daeploy/cli/service_template/{{cookiecutter.project_name}}/service.py
+++ daeploy/cli/service_template/{{cookiecutter.project_name}}/service.py
@@ -3,11 +3,15 @@ from daeploy import service
 
 logger = logging.getLogger(__name__)
 
+service.add_parameter("greeting_phrase", "Hello")
+
 
 @service.entrypoint
 def hello(name: str) -> str:
+    greeting_phrase = service.get_parameter("greeting_phrase")
+
     logger.info(f"Greeting someone with the name: {name}")
-    return f"Hello {name}"
+    return f"{greeting_phrase} {name}"
 
 
 if __name__ == "__main__":
